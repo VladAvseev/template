1fa25846dfa2594a8324f992c3f099e2127a5218
Tulen41k <tu1en41k@yandex.ru>
2024-03-22 21:03:50 +0300

    pupupuuu

--- src/modules/_task/_create/index.tsx
+++ src/modules/_task/_create/index.tsx
@@ -1,18 +1,43 @@
-// import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/styles";
 import { createTask } from "./models";
-import { useEffect } from "react";
+import { useEffect} from "react";
+import { Paper } from "@mui/material";
+import VTextField from "../../../mvvm/TextField/VTextField";
+import VButton from "../../../mvvm/Button/VButton";
+import VSelect from "../../../mvvm/Select/VSelect";
+import VDatePicker from "../../../mvvm/DatePicker/VDatePicker";
 
-// const useStyles = makeStyles(() => ({
-// }));
+
+ const useStyles = makeStyles(() => ({
+	 taskForm: {
+		padding: "20px",
+		maxWidth: "400px", 
+		margin: "0 auto"
+	} 
+ }));
 
 const CreateTaskPage: React.FC = () => {
-	const { start } = createTask;
+	const { start, titleText, createBtn, taskResponsibleSelect, descriptionText, taskStatusSelect, dateSelect, taskSelect, connectionSelect, setConnectionBtn} = createTask;
+
 
 	useEffect(() => {
 		start();
 	}, []);
 
-	// const styles = useStyles();
-	return <>CREATE TASK PAGE</>
+	const styles = useStyles();
+	return (
+		<Paper className={styles.taskForm} elevation={3} >
+			<VTextField model={titleText}/>
+			<VTextField model={descriptionText}/>
+			<VSelect model={taskStatusSelect}/>
+			<VSelect model={taskResponsibleSelect}/>                                       	
+			<VDatePicker model={dateSelect}/>
+			<VSelect model={taskSelect}/>
+			<VSelect model={connectionSelect}/>
+			<VButton model={setConnectionBtn}/>                               
+			<VButton model={createBtn}/>
+  		</Paper> 
+		);
+		
 };
 export default CreateTaskPage;
--- src/modules/_task/_create/models/index.ts
+++ src/modules/_task/_create/models/index.ts
@@ -1,13 +1,130 @@
 import { types } from "mobx-state-tree";
+import VMTextField from "../../../../mvvm/TextField/VMTextField";
+import VMButton from "../../../../mvvm/Button/VMButton";
+import VMSelect from "../../../../mvvm/Select/VMSelect";
+import MSelectOption from "../../../../mvvm/Select/MSelectOption";
+import VMDatePicker from "../../../../mvvm/DatePicker/VMDatePicker";
 
 export const createTask = types.model('createTask')
 .volatile(() => ({
 	// здесь будут модели компонентов страницы
+	titleText: VMTextField.create({
+		label: "Название задачи"
+	}),
+	descriptionText : VMTextField.create({
+		label: "Описание задачи"
+	}),
+	taskStatusSelect: VMSelect.create({
+		options: [MSelectOption.create ({
+			label: "Создана",
+			value: "Создана",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "Назначен ответственный",
+			value: "Назначен ответственный",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "В процессе выполнения",
+			value: "В процессе выполнения",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "На проверке",
+			value: "На проверке",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "Выполнена",
+			value: "Выполнена",
+			isSelected: false,
+			isDisabled: false,
+		})]
+	}),
+	taskResponsibleSelect: VMSelect.create({
+		options: [MSelectOption.create ({
+			label: "Валя",
+			value: "Валя",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "Влад",
+			value: "Влад",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "Юра",
+			value: "Юра",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "Артем",
+			value: "Артем",
+			isSelected: false,
+			isDisabled: false,
+		}), MSelectOption.create ({
+			label: "Вика",
+			value: "Вика",
+			isSelected: false,
+			isDisabled: false,
+		})]
+	}),
+	dateSelect: VMDatePicker.create({}),
+	createBtn: VMButton.create({
+		text: "Создать задачу"
+	}),
+	taskSelect: VMSelect.create({
+		options: [
+		MSelectOption.create ({
+			label: "Создать фичу",
+			value: "Создать фичу",
+			isSelected: false,
+			isDisabled: false,
+		}),
+		MSelectOption.create ({
+			label: "Пофиксить баг",
+			value: "Пофиксить баг",
+			isSelected: false,
+			isDisabled: false,
+		})
+		]
+	}),
+	connectionSelect: VMSelect.create({
+		options: [
+			MSelectOption.create ({
+				label: "Дочерняя",
+				value: "Дочерняя",
+				isSelected: false,
+				isDisabled: false,
+			}),
+			MSelectOption.create ({
+				label: "Родительская",
+				value: "Родительская",
+				isSelected: false,
+				isDisabled: false,
+			})
+			]
+	}),
+	setConnectionBtn: VMButton.create({
+		text: "Сохранить связь"
+	})
 }))
-.views(() => ({
-
+.views((self) => ({
+	get data() {
+		return {taskName: self.titleText.value,
+				description: self.descriptionText.value,
+				taskSelect: self.taskResponsibleSelect.selected,
+				taskStatus: self.taskStatusSelect.selected,
+				taskDate: self.dateSelect.value
+			}
+	}
 }))
-.actions(() => ({
+.actions((self) => ({
+	afterCreate(){
+		self.createBtn.setOnClick(() => {console.log(self.data)}),
+		self.setConnectionBtn.setOnClick(() => {})
+	}
 	// здесь другие методы страницы
 }))
 .actions(() => ({
