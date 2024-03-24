6ef69c1c7d77360325dde4ae3ee44cb515b59b90
KimYuriy <komrad.kim-j@yandex.ru>
2024-03-24 13:15:34 +0300

    Finish for Yuriy Kim's work

--- src/modules/_dashboard/components/task_component/TaskComponent.tsx
+++ src/modules/_dashboard/components/task_component/TaskComponent.tsx
@@ -3,6 +3,7 @@ import c from "./TaskComponent.module.css"
 import { TTaskInstance } from "../../models/task"
 import { Avatar, Chip, Paper, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
+import { WarningIcon } from "../../../../components/WarningIcon"
 
 type props = {
   task: TTaskInstance
@@ -12,7 +13,12 @@ export const TaskComponent: React.FC<props> = ({ task }) => {
   return (
     <Link to={`/edit_task/${task.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
       <Paper className={c.taskComponent}>
-        <Typography><strong>{task.title}</strong></Typography>
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between'}}>
+          <Typography><strong>{task.title}</strong></Typography>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between', gap: '2px' }}>
+            {task.warnings.map((warning) => <WarningIcon key={Math.random() + Math.random()} type={warning.type} task_id={warning.task_id} />)}
+          </div>
+        </div>
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', }}>
           <Chip
             label={task.responsible.username}
